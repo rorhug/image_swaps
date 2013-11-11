8258fdb391cbf3d4349a9e246f9ca5e38f3e0307
Rory Hughes <rory@roryhughes.net>
2013-11-11 23:12:53 +0000

    added home controller

---
+++ controllers/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports.index = function(req, res){
+  res.sendfile("public/index.html");
+};
+
+exports.changes = function(req, res){
+	res.type('application/json');
+  res.sendfile("changelog.json");
+};
